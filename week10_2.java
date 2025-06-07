5f7ac118646ec554c053c462da632821b222f28f
pranathibankuru <pranathidnv@gmail.com>
2025-06-07 21:56:20 +0530

    Add files via upload

---
+++ week10_2.java
@@ -0,0 +1,12 @@
+public class week10_2 {
+    public static void main(String[] args) {
+        System.out.println("x: B.Pranathi, 24022");
+
+        String string = "Welcome! You are practicing strings concepts.";
+
+        System.out.println("Capital letters: " + string.toUpperCase());
+        System.out.println("Lower case: " + string.toLowerCase());
+        System.out.println("Length: " + string.length());
+        System.out.println("Index: " + string.indexOf(string));
+    }
+}
